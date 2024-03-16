da09fb65dbe05d7533f70ffa6bbefa9d31948b88
silyface <110118193+silyface@users.noreply.github.com>
2024-03-17 00:55:27 +0800

    WASD移动代码

---
+++ Playercontroller.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{
+    public float moveSpeed = 5f; 
+
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0,0);
+
+        if(Input.GetKey(KeyCode.W))
+        {inputVector.y=+1;}
+        if(Input.GetKey(KeyCode.S))
+        {inputVector.y=-1;}
+        if(Input.GetKey(KeyCode.A))
+        {inputVector.x=-1;}
+        if(Input.GetKey(KeyCode.D))
+        {inputVector.x=+1;}
+
+        inputVector =inputVector.normalized;
+
+        Vector3 moveDoir = new Vector3(inputVector.x,0f,inputVector.y);
+        transform.position+= moveDoir*moveSpeed*Time.deltaTime;
+
+        Debug.Log(Time.deltaTime);
+    
+    }
+}
